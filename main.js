7005f3f2954ab0abb6e9995a59f71739c43b7dc4
Maxim <max.m.ivanoff@gmail.com>
2025-08-13 15:58:34 +0300

    Changed "contentBox.js" to "body.js"

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import Wrapper from "./constructor/Wrapper.js";
 import sidebar from "./modules/sidebar.js";
-import body from "./modules/contentBox.js";
+import body from "./modules/body.js";
 
 new Wrapper('#root', [body, sidebar]);
